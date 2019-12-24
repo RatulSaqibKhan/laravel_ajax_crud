434c4c2ab7a2ea0fab61f0a666adf5fe021faec7
Saqibkhan <saqib@skylarksoft.com>
2019-12-24 16:36:44 +0600

    Navigation Menus after logged in

---
+++ resources/views/includes/logged_in_navigation_bar.blade.php
@@ -0,0 +1,64 @@
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        Students <span class="caret"></span>
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ url('/students') }}">
+            List
+        </a>
+        <a class="dropdown-item" href="{{ url('/students/create') }}">
+            New Student
+        </a>
+    </div>
+</li>
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        Classes <span class="caret"></span>
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ url('/student-classes') }}">
+            List
+        </a>
+        <a class="dropdown-item" href="{{ url('/student-classes/create') }}">
+            New Class
+        </a>
+    </div>
+</li>
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        Sections <span class="caret"></span>
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ url('/sections') }}">
+            List
+        </a>
+        <a class="dropdown-item" href="{{ url('/sections/create') }}">
+            New Section
+        </a>
+    </div>
+</li>
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link" href="{{ url('/assign-student-in-class') }}">
+        Assign Student in Class
+    </a>
+</li>
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        {{ Auth::user()->name }} <span class="caret"></span>
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ route('logout') }}"
+           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+    </div>
+</li>
